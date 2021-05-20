e9fe5407766f23268dd3a1cff646d15744bbae53
charley-d <67061817+charley-d@users.noreply.github.com>
2021-05-20 12:10:52 +0100

    Create initials.cpp

---
+++ initials.cpp
@@ -0,0 +1,18 @@
+// Charley D
+// My new fave animal is the manatee
+
+#include <iostream>
+
+int main() {
+
+std::cout << " CCC  DDDD \n";
+std::cout << "C   C D   D\n";
+std::cout << "C     D   D\n";
+std::cout << "C     D   D\n";
+std::cout << "C     D   D\n";
+std::cout << "C   C D   D\n";
+std::cout << " CCC  DDDD \n";
+
+return 0;
+
+}
